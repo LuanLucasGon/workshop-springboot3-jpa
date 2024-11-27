71433941622979fb666ab47664156878e4e1ba1a
LuanLucasGon <lucasluangon@gmail.com>
2024-11-27 17:28:58 -0300

    feat: Creating User entity and UserResource

---
+++ src/main/java/com/example/course/entities/User.java
@@ -0,0 +1,21 @@
+package com.example.course.entities;
+
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
