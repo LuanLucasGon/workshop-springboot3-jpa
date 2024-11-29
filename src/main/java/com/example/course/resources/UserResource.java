12a9f5b5ac81e47523be0899ce19cef3f5153fe0
LuanLucasGon <lucasluangon@gmail.com>
2024-11-29 10:55:38 -0300

    feat: creating service layer

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,19 +1,31 @@
 package com.example.course.resources;
 
 import com.example.course.entities.User;
+import com.example.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Maria", "Maria@gmail.com", "999999999", "12345");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id ){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
