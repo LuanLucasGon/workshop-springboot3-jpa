4f148aaea29f5054b3859ee9487a0dc05d87beb0
LuanLucasGon <lucasluangon@gmail.com>
2024-11-29 10:07:15 -0300

    feat: installing h2 database and creating test profile

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.example.course.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serial;
@@ -10,9 +11,14 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
