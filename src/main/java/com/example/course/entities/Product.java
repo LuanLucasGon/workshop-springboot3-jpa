a3ecdaf351b8205d497ae77390f8235a333605a8
LuanLucasGon <lucasluangon@gmail.com>
2024-12-02 10:29:51 -0300

    feat: creating relation order and product

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -1,42 +1,53 @@
 package com.example.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-@Getter
-@Setter
-@EqualsAndHashCode
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_products")
 public class Product implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Getter
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private String description;
+    @Getter
+    @Setter
     private Double price;
+    @Getter
+    @Setter
     private String imgUrl;
 
+    @Getter
     @ManyToMany
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns =@JoinColumn(name = "category_id")
     )
-    @Getter
     private Set<Category> categories = new HashSet<>();
 
-    public Product() {
-    }
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items= new HashSet<>();
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -44,5 +55,27 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> orderSet = new HashSet<>();
+        for (OrderItem orderItem: items){
+            orderSet.add(orderItem.getOrder());
+        }
+        return orderSet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }
